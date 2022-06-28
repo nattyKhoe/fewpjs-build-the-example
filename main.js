4eca36b392f6269e5c4f55c5c0bbaeb9e4a461d1
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-28 21:06:57 +1000

    commit tree 3eb199ee8074b23fa270a5457611111c60b4b8d6

--- main.js
+++ main.js
@@ -4,22 +4,48 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+window.onload = function(){
 
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
+  const likeBtn = document.querySelectorAll('.like-glyph');
+  errorMsg = document.querySelector('#modal');
+  function popUpMsg () {
+    errorMsg.className = "hidden";
+  }
+  document.addEventListener("click", function(e){
+    if (e.target.tagName === 'SPAN' ){
+      if (e.target.innerText === EMPTY_HEART){
+        mimicServerCall()
+        .then(response => {
+          e.target.innerText = FULL_HEART;
+          e.target.className = "activated-heart";
+        })
+        .catch(error => {
+          errorMsg.classList.remove("hidden");
+          setTimeout(popUpMsg, 3000);
+        });
+      } else if (e.target.innerText===FULL_HEART) {
+        e.target.classList.remove ("activated-heart");
+        e.target.className = 'like-glyph';
+        e.target.innerText = EMPTY_HEART;
       }
-    }, 300);
+    }
   });
-}
+
+
+  //------------------------------------------------------------------------------
+  // Don't change the code below: this function mocks the server response
+  //------------------------------------------------------------------------------
+
+  function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        let isRandomFailure = Math.random() < .2
+        if (isRandomFailure) {
+          reject("Random server error. Try again.");
+        } else {
+          resolve("Pretend remote server notified of action!");
+        }
+      }, 300);
+    });
+  }
+};
